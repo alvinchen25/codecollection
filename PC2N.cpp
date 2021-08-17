78de98773eadb1b39fe773fb694dc769d7995daf
alvinchen25 <81381172+alvinchen25@users.noreply.github.com>
2021-08-17 09:59:50 -0400

    Add files via upload

---
+++ PC2N.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+
+using namespace std;
+long long YEET=1000000007;
+
+int main() {
+
+}
